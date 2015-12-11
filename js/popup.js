44eaf575dc02e21e44f8c1fc5539ce74f81c2c7a
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-12-11 21:10:58 +0900

    youtube search

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,12 @@
-$(function () {
-    chrome.tabs.getSelected(null, function(tab) {
-        var a = $('#title').text(tab.title.split("-")[0]);
-		//$('#url').text(tab.url.split("(")[1].split(")")[0].split(",")[0]); 		//family name
-		//$('#url').text(tab.url.split("(")[1].split(")")[0]).split(",")[1]); 		//first name    familyとfirstを両方有効にすると表示できない。
-    });
-});
+window.onload = function() {
+	chrome.tabs.query( {active: true, lastFocusedWindow: true}, function (tabs) {
+		var tab = tabs[0];
+		var music_title = tab.title.split("(")[0];
+		var composer = tab.title.split("(")[1].split(")")[0].split(",")[0];
+		//tab.urlに開いているタブのURLが入っている
+		var search = "https://www.youtube.com/results?search_query=" + composer + "+" + music_title;
+		//chrome.tabs.update(tab.id, {url: search})
+		chrome.tabs.create({url: search});
+		window.close();
+	});
+}
