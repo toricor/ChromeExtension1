4f4229d1eb0cf8bfb386895e0b8d63812b713ed6
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-12-11 21:20:40 +0900

    small change

--- js/popup.js
+++ js/popup.js
@@ -3,9 +3,7 @@ window.onload = function() {
 		var tab = tabs[0];
 		var music_title = tab.title.split("(")[0];
 		var composer = tab.title.split("(")[1].split(")")[0].split(",")[0];
-		//tab.urlに開いているタブのURLが入っている
 		var search = "https://www.youtube.com/results?search_query=" + composer + "+" + music_title;
-		//chrome.tabs.update(tab.id, {url: search})
 		chrome.tabs.create({url: search});
 		window.close();
 	});
