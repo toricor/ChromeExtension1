2df0707c95036123b0370c7e60659b93cc960d14
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-12-11 21:25:35 +0900

    add first_name

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,8 @@ window.onload = function() {
 		var tab = tabs[0];
 		var music_title = tab.title.split("(")[0];
 		var composer = tab.title.split("(")[1].split(")")[0].split(",")[0];
-		var search = "https://www.youtube.com/results?search_query=" + composer + "+" + music_title;
+		var first_name = tab.title.split("(")[1].split(")")[0].split(",")[1];
+		var search = "https://www.youtube.com/results?search_query=" + composer+first_name+ "+" + music_title + "+"   ;
 		chrome.tabs.create({url: search});
 		window.close();
 	});
